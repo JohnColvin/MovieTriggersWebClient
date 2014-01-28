9508576c2c8a5aaa2445c0898eebdaa31a2bfae6
John Colvin <john.colvin@neo.com>
2014-01-27 23:28:38 -0500

    Adds index and show for titles

--- api-stub/routes.js
+++ api-stub/routes.js
@@ -1,29 +1,35 @@
 module.exports = function(server) {
 
-  // Create an API namespace, so that the root does not 
+  // Create an API namespace, so that the root does not
   // have to be repeated for each end point.
 	server.namespace('/api', function() {
 
-		// Return fixture data for '/api/posts/:id'
-		server.get('/posts/:id', function(req, res) {
-			var post = {
-					  "post": {
-					    "id": 1,
-					    "title": "Rails is omakase",
-					    "comments": ["1", "2"],
-					    "user" : "dhh"
-					  },
+		server.get('/titles', function(req, res) {
+			var titles = {
+			  "titles": [
+			  	{
+				    "id": 1,
+				    "name": "Up"
+				  },
+				  {
+				  	"id": 2,
+				  	"name": "Blade Runner"
+				  }
+			  ]
+			}
+			res.send(titles);
+		});
 
-					  "comments": [{
-					    "id": "1",
-					    "body": "Rails is unagi"
-					  }, {
-					    "id": "2",
-					    "body": "Omakase O_o"
-					  }]
+		// Return fixture data for '/api/posts/:id'
+		server.get('/titles/:id', function(req, res) {
+			var title = {
+					  "title": {
+					    "id": 1,
+					    "name": "Up"
+					  }
 					};
 
-			res.send(post);
+			res.send(title);
 		});
 
 	});
