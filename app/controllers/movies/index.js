b73fa1e5ae11e0265d1cfd5aace16bd40f3e6097
John Colvin <john.colvin@neo.com>
2014-02-08 11:33:01 -0500

    Moves logic of searching message from view to controller

--- app/controllers/movies/index.js
+++ app/controllers/movies/index.js
@@ -27,6 +27,20 @@ export default Ember.ArrayController.extend({
     this.set('searchInProgress', false);
   },
 
+  searchMessage: function () {
+    var prefix = '';
+    if (this.get('searchInProgress')) {
+      prefix = 'Searching';
+    } else {
+      if (this.get('content.length')) {
+        prefix = 'Results';
+      } else {
+        prefix = 'No results';
+      }
+    }
+    return prefix + ' for movies titled ';
+  }.property('searchInProgress', 'content.length'),
+
   actions: {
     goToMovie: function (id) {
       this.transitionToRoute('movies.movie', id);
